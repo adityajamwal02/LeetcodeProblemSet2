20e002b0b170765b36114127b38905941d36442f
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-05-28 11:26:15 -0700

    Add files via upload

---
+++ RemoveTrailingZerosFromAString.cpp
@@ -0,0 +1,30 @@
+/*
+Given a positive integer num represented as a string, return the integer num without trailing zeros as a string.
+
+Example 1:
+Input: num = "51230100"
+Output: "512301"
+Explanation: Integer "51230100" has 2 trailing zeros, we remove them and return integer "512301".
+
+Example 2:
+Input: num = "123"
+Output: "123"
+Explanation: Integer "123" has no trailing zeros, we return integer "123".
+*/
+
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        int n=num.size(), i=n-1;
+        while(i>=0){
+            if(i==0 and num[i]==0) return "0";
+            else if(num[i]=='0'){
+                i--;
+            }else if(num[i]!='0'){
+                string result=num.substr(0,i+1);
+                return result;
+            }
+        }
+    return "0";
+    }
+};
