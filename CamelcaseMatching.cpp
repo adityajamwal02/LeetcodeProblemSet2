d4d242aac1bd19ebfdd99152be66fd422fdae415
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-05-29 16:14:37 +0530

    Add files via upload

---
+++ CamelcaseMatching.cpp
@@ -0,0 +1,48 @@
+/*
+Given an array of strings queries and a string pattern, return a boolean array answer where answer[i] is true if queries[i] matches pattern, and false otherwise.
+A query word queries[i] matches pattern if you can insert lowercase English letters pattern so that it equals the query. You may insert each character at any position and you may not insert any characters.
+
+Example 1:
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB"
+Output: [true,false,true,true,false]
+Explanation: "FooBar" can be generated like this "F" + "oo" + "B" + "ar".
+"FootBall" can be generated like this "F" + "oot" + "B" + "all".
+"FrameBuffer" can be generated like this "F" + "rame" + "B" + "uffer".
+
+Example 2:
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBa"
+Output: [true,false,true,false,false]
+Explanation: "FooBar" can be generated like this "Fo" + "o" + "Ba" + "r".
+"FootBall" can be generated like this "Fo" + "ot" + "Ba" + "ll".
+
+Example 3:
+Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBaT"
+Output: [false,true,false,false,false]
+Explanation: "FooBarTest" can be generated like this "Fo" + "o" + "Ba" + "r" + "T" + "est".
+*/
+
+class Solution {
+public:
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        vector<bool> result;
+        int m=pattern.size();
+        for(auto query: queries){
+            int n=query.size(), i=0,j=0;
+            while(i<n){
+                if(query[i]==pattern[j] and j<m){
+                    i++;
+                    j++;
+                }else if((int)query[i]<97){
+                    break;
+                }else{
+                    i++;
+                }
+            }
+            if(j==m and i==n)
+                result.push_back(true);
+            else
+                result.push_back(false);
+        }
+    return result;
+    }
+};
