0f6cc05f4fb916c2b0f3dc5396e075b945a6cc80
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-06-20 14:19:49 +0530

    Add files via upload

---
+++ MixTheColors.cpp
@@ -0,0 +1,22 @@
+/// Codechef : Problem Code: MIXCOLOR
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        set<int> st(arr.begin(),arr.end());
+        cout<<n-st.size()<<endl;
+    }
+	return 0;
+}
+
