b39d1f5405f66096787f7723f2e8a4ab4a924696
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-08-27 04:59:11 +0530

    Add files via upload

---
+++ CountIntegersInIntervals.cpp
@@ -0,0 +1,71 @@
+/*
+Given an empty set of intervals, implement a data structure that can:
+
+Add an interval to the set of intervals.
+Count the number of integers that are present in at least one interval.
+Implement the CountIntervals class:
+
+CountIntervals() Initializes the object with an empty set of intervals.
+void add(int left, int right) Adds the interval [left, right] to the set of intervals.
+int count() Returns the number of integers that are present in at least one interval.
+Note that an interval [left, right] denotes all the integers x where left <= x <= right.
+
+Example 1:
+Input
+["CountIntervals", "add", "add", "count", "add", "count"]
+[[], [2, 3], [7, 10], [], [5, 8], []]
+Output
+[null, null, null, 6, null, 8]
+
+Explanation
+CountIntervals countIntervals = new CountIntervals(); // initialize the object with an empty set of intervals.
+countIntervals.add(2, 3);  // add [2, 3] to the set of intervals.
+countIntervals.add(7, 10); // add [7, 10] to the set of intervals.
+countIntervals.count();    // return 6
+                           // the integers 2 and 3 are present in the interval [2, 3].
+                           // the integers 7, 8, 9, and 10 are present in the interval [7, 10].
+countIntervals.add(5, 8);  // add [5, 8] to the set of intervals.
+countIntervals.count();    // return 8
+                           // the integers 2 and 3 are present in the interval [2, 3].
+                           // the integers 5 and 6 are present in the interval [5, 8].
+                           // the integers 7 and 8 are present in the intervals [5, 8] and [7, 10].
+                           // the integers 9 and 10 are present in the interval [7, 10].
+
+*/
+
+class CountIntervals {
+public:
+    map<int,int> mp;
+    int counter=0;
+    CountIntervals() {
+    }
+    void add(int left, int right){
+        auto pos=mp.upper_bound(left); // This takes O(logn) compared to normal upper bound which takes O(n)
+        if(pos!=mp.begin()){
+            pos--;
+            if(left>pos->second){
+                pos++;
+            }
+        }
+        while(pos!=mp.end() and pos->first<=right){
+            left=min(left,pos->first);
+            right=max(right,pos->second);
+            counter-=(pos->second-pos->first+1);
+            auto temp=pos++;
+            mp.erase(temp);
+        }
+        mp[left]=right;
+        counter+=(right-left+1);
+    }
+
+    int count() {
+        return counter;
+    }
+};
+
+/**
+ * Your CountIntervals object will be instantiated and called as such:
+ * CountIntervals* obj = new CountIntervals();
+ * obj->add(left,right);
+ * int param_2 = obj->count();
+ */
