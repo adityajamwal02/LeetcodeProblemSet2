ff92c594603de1b2451bade06aee510c383d3066
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-10-27 02:08:16 +0530

    Add files via upload

---
+++ SmallestStringStartingFromLeaf.cpp
@@ -0,0 +1,52 @@
+/*
+You are given the root of a binary tree where each node has a value in the range [0, 25] representing the letters 'a' to 'z'.
+Return the lexicographically smallest string that starts at a leaf of this tree and ends at the root.
+As a reminder, any shorter prefix of a string is lexicographically smaller.
+For example, "ab" is lexicographically smaller than "aba".
+A leaf of a node is a node that has no children.
+
+Example 1:
+Input: root = [0,1,2,3,4,3,4]
+Output: "dba"
+
+Example 2:
+Input: root = [25,1,3,1,3,0,2]
+Output: "adz"
+
+Example 3:
+Input: root = [2,2,1,null,1,0,null,0]
+Output: "abc"
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode *root, string &result, string temp){
+        if(!root) return;
+        temp=(char)(root->val+'a')+temp;
+        if(!root->left and !root->right){
+            if(result=="" or result.compare(temp)>0){
+                result=temp;
+                return;
+            }
+        }
+        helper(root->left,result,temp);
+        helper(root->right,result,temp);
+    return;
+    }
+    string smallestFromLeaf(TreeNode* root){
+        string result;
+        helper(root,result,"");
+    return result;
+    }
+};
